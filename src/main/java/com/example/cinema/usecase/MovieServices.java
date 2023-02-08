e2c328a08629c4538d189bbafe272127a90d333f
Brian Vanegas <brianvs18mail.com>
2023-02-07 20:10:25 -0500

    se realiza refactorizacion de codigo

--- src/main/java/com/example/cinema/usecase/MovieServices.java
+++ src/main/java/com/example/cinema/usecase/MovieServices.java
@@ -1,21 +1,18 @@
 package com.example.cinema.usecase;
 
 import com.example.cinema.dto.MovieDTO;
-import com.example.cinema.dto.ShowtimeDTO;
 import com.example.cinema.enums.GenericErrorEnum;
 import com.example.cinema.enums.MovieErrorEnum;
 import com.example.cinema.exceptions.GenericException;
 import com.example.cinema.exceptions.MovieException;
 import com.example.cinema.factory.MovieFactory;
 import com.example.cinema.repository.MovieRepository;
-import com.example.cinema.repository.ShowtimeRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -23,7 +20,7 @@ import java.util.Objects;
 public class MovieServices implements MovieFactory {
 
     private final MovieRepository movieRepository;
-    private final ShowtimeRepository showtimeRepository;
+    private final ShowtimeServices showtimeServices;
     private static final Integer MAXIMUM_RANGE = 5;
 
     public Flux<MovieDTO> findAll() {
@@ -33,11 +30,6 @@ public class MovieServices implements MovieFactory {
 
     public Mono<MovieDTO> findById(String movieId) {
         return movieRepository.findById(movieId)
-                .map(this::buildMovieDTO);
-    }
-
-    public Flux<MovieDTO> findByIdIn(List<String> movieId) {
-        return movieRepository.findAllById(movieId)
                 .map(this::buildMovieDTO);
     }
 
@@ -60,20 +52,8 @@ public class MovieServices implements MovieFactory {
     }
 
     public Mono<Void> deleteMovie(String movieId) {
-        return findByMovieId(movieId)
+        return showtimeServices.findByMovieId(movieId)
                 .switchIfEmpty(movieRepository.deleteById(movieId).then(Mono.empty()))
                 .then();
     }
-
-    public Flux<ShowtimeDTO> findByMovieId(String movieId) {
-        return showtimeRepository.findAll()
-                .flatMap(showtimeDocument -> Flux.fromIterable(showtimeDocument.getMovies())
-                        .filter(moviesIds-> moviesIds.equals(movieId))
-                        .map(showtime -> ShowtimeDTO.builder()
-                                .id(showtimeDocument.getId())
-                                .date(showtimeDocument.getDate())
-                                .movies(showtimeDocument.getMovies())
-                                .build()));
-    }
-
 }
--- src/main/java/com/example/cinema/usecase/common/functions/MovieListFunction.java
+++ src/main/java/com/example/cinema/usecase/common/functions/MovieListFunction.java
@@ -1,7 +1,7 @@
 package com.example.cinema.usecase.common.functions;
 
 import com.example.cinema.dto.MovieDTO;
-import com.example.cinema.usecase.MovieServices;
+import com.example.cinema.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -11,11 +11,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class MovieListFunction {
-    private final MovieServices movieServices;
+    private final MovieRepository movieRepository;
 
     public Mono<List<MovieDTO>> mapMoviesList(List<String> movies) {
-        return movieServices.findByIdIn(movies)
-                .map(movieDTO -> movieDTO.toBuilder()
+        return movieRepository.findAllById(movies)
+                .map(movieDTO -> MovieDTO.builder()
                         .id(movieDTO.getId())
                         .title(movieDTO.getTitle())
                         .director(movieDTO.getDirector())
